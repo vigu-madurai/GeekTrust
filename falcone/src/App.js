faa9dadda91e2c5488ba7ae1c8d7ce672b503421
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-07-15 22:45:44 +0530

    header, footer - css setup

--- falcone/src/App.js
+++ falcone/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
 
+// Business-logic code flow starts here
 function App() {
-	return <div>Initial Setup</div>;
+	return (
+		<div className='falcone-container'>
+			{/* Header */}
+			<header className='header inner-padding'>
+				<div className='header-title'>Finding Falcone!</div>
+				<div className='header-btns'>
+					<div>Reset</div>
+					<div>GeekTrust Home</div>
+				</div>
+			</header>
+			{/* Body */}
+			<div className='body-container inner-padding'>Body</div>
+			{/* Footer */}
+			<footer className='footer inner-padding'>Coding Problem | GeekTrust Challenge</footer>
+		</div>
+	);
 }
 
 export default App;
