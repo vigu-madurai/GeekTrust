fc5760668d17230badc8311bcc0e330a7f5d1f81
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-07-23 00:24:47 +0530

    added images

--- falcone/src/constants/image-constants.js
+++ falcone/src/constants/image-constants.js
@@ -5,6 +5,18 @@ import Lerbin from "../assets/Lerbin.png";
 import Pingasor from "../assets/Pingasor.png";
 import Sapir from "../assets/Sapir.png";
 
+import SpacePod from "../assets/SpacePod.gif";
+import SpaceRocket from "../assets/SpaceRocket.gif";
+import SpaceShip from "../assets/SpaceShip.gif";
+import SpaceShuttle from "../assets/SpaceShuttle.gif";
+
+export const vehicleImgConstant = {
+  "Space pod": SpacePod,
+  "Space rocket": SpaceRocket,
+  "Space ship": SpaceShip,
+  "Space shuttle": SpaceShuttle
+};
+
 export const planetImgConstant = {
   Donlon: Donlon,
   Enchai: Enchai,
