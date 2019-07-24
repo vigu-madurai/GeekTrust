299ae04b81c5d00cbbb25b11cc5edd3d26cd209e
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-07-25 01:06:31 +0530

    css revamp at home

--- falcone/src/App.js
+++ falcone/src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import './App.css';
-import Home from './modules/Home';
+import React from "react";
+import "./App.css";
+import Home from "./modules/Home";
 
 // Business-logic code flow starts here
 function App() {
-	return (
-		<div className='falcone-container'>
-			{/* Header */}
-			<header className='header inner-padding'>
-				<div className='header-title'>Finding Falcone!</div>
-				<div className='header-btns'>
-					<div>Reset</div>
-					<div>GeekTrust Home</div>
-				</div>
-			</header>
-			{/* Body */}
-			<div className='body-container inner-padding'>
-				<Home />
-			</div>
-			{/* Footer */}
-			<footer className='footer inner-padding'>Coding Problem | GeekTrust Challenge</footer>
-		</div>
-	);
+  return (
+    <div className="falcone-container">
+      {/* Header */}
+      <header className="header inner-padding">
+        <div className="header-title">Finding Falcone!</div>
+        <a href="/" className="header-btns">
+          Home
+        </a>
+      </header>
+      {/* Body */}
+      <div className="body-container inner-padding">
+        <Home />
+      </div>
+      {/* Footer */}
+      <footer className="footer inner-padding">
+        Coding Problem | GeekTrust Challenge
+      </footer>
+    </div>
+  );
 }
 
 export default App;
