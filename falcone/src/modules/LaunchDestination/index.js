c2cf0bbdbac8a4d5c051ac034df4843a16c25e9a
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-07-25 01:33:09 +0530

    added buttons

--- falcone/src/modules/LaunchDestination/index.js
+++ falcone/src/modules/LaunchDestination/index.js
@@ -1,73 +1,142 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 
-import { setCurrentDestination, setSelectedPlanets } from "../../actions";
+import {
+  setCurrentDestination,
+  setSelectedPlanets,
+  setSelectedVehicles,
+  setTimeTaken
+} from "../../actions";
 import Planets from "../Planets";
+import Results from "../Results";
+import Vehicles from "../Vehicles";
+import launchToPlanets from "../../assets/launchToPlanets.gif";
+import loader from "../../assets/loader.gif";
 import "./index.css";
 
 class LaunchDestination extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      activePlanet: null,
+      activeVehicle: null,
+      timeValue: props.timeTaken
+    };
   }
 
   setCurrentPlanet = currentPlanet => {
     // add only the current selected planet for the current launch
-    this.props.setSelectedPlanets(currentPlanet);
+    this.setState({ activePlanet: currentPlanet, activeVehicle: null });
   };
 
-  filterPlanets = () => {
-    // remove the launched planets
-    const { planets, selectedPlanets } = this.props;
-    return planets.filter(el => {
-      return !selectedPlanets.includes(el.name);
-    });
+  setCurrentVehicle = currentVehicle => {
+    this.setState({ activeVehicle: currentVehicle });
   };
 
+  setTimeTaken = timeValue => {
+    this.setState({ timeValue: timeValue });
+  };
   render() {
-    const { currentDestination } = this.props;
+    const {
+      currentDestination,
+
+      results,
+      planets
+    } = this.props;
     return (
       <>
         <div className="destination-container-heading inner-padding">
-          Select Planets you want to search in...
+          Select any four planets you want to search in...
         </div>
-        <div className="destination-container">
-          {/* run the launch vehicles for 4 destinations */}
-          {currentDestination < 4 ? (
-            <div className="launch-wrapper">
-              <div className="planet-wrapper">
-                {/* show the available planets */}
-                <Planets
-                  setCurrentPlanet={this.setCurrentPlanet}
-                  availablePlanets={this.filterPlanets()}
-                />
+        {!Object.keys(planets).length ? (
+          <div className="loader-wrapper">
+            <img src={loader} alt="loader" />
+            Loading...
+          </div>
+        ) : (
+          <div className="destination-container">
+            {/* run the launch vehicles for 4 destinations */}
+            {currentDestination < 4 ? (
+              <div className="launch-wrapper">
+                <div className="planet-wrapper">
+                  {/* show the available planets */}
+                  <Planets
+                    setCurrentPlanet={this.setCurrentPlanet}
+                    activePlanet={this.state.activePlanet}
+                  />
+                </div>
+                {this.state.activePlanet && (
+                  <Vehicles
+                    currentPlanet={this.state.activePlanet}
+                    setCurrentVehicle={this.setCurrentVehicle}
+                    setTimeTaken={this.setTimeTaken}
+                    timeValue={this.state.timeValue}
+                  />
+                )}
+
+                {this.state.activePlanet && this.state.activeVehicle && (
+                  <div className="btn-containers">
+                    <button
+                      onClick={() =>
+                        this.setState({
+                          activePlanet: null,
+                          activeVehicle: null
+                        })
+                      }
+                      className="reset-btn"
+                    >
+                      Reset
+                    </button>
+                    <button
+                      onClick={() => {
+                        this.props.setSelectedPlanets(this.state.activePlanet);
+                        this.props.setSelectedVehicles(
+                          this.state.activeVehicle
+                        );
+                        this.props.setCurrentDestination();
+                        this.setState({
+                          activePlanet: null,
+                          activeVehicle: null
+                        });
+                      }}
+                      className="launch-btn"
+                    >
+                      Launch
+                    </button>
+                  </div>
+                )}
               </div>
-              <div className="vehicles-wrapper">Vehicles</div>
-              <button
-                onClick={() => {
-                  this.props.setCurrentDestination();
-                }}
-              >
-                Launch Destination {currentDestination + 1}
-              </button>
-            </div>
-          ) : (
-            // result goes here
-            "Here the result is"
-          )}
-        </div>
+            ) : // result goes here
+            results ? (
+              <Results />
+            ) : (
+              <div className="loader-wrapper">
+                <img src={launchToPlanets} alt="loader" />
+                Launching...
+              </div>
+            )}
+          </div>
+        )}
       </>
     );
   }
 }
 
-const mapDispatchToProps = { setCurrentDestination, setSelectedPlanets };
+const mapDispatchToProps = {
+  setCurrentDestination,
+  setSelectedPlanets,
+  setSelectedVehicles,
+  setTimeTaken
+};
 
 const mapStateToProps = state => {
   return {
     planets: state.planets,
     currentDestination: state.currentDestination,
-    selectedPlanets: state.selectedPlanets
+    selectedPlanets: state.selectedPlanets,
+    selectedVehicles: state.selectedVehicles,
+    results: state.results,
+    timeTaken: state.timeTaken
   };
 };
 
