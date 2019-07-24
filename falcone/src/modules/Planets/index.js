fe368de3ba7a7fbfe0152a70dcb4e00ced8f6d1d
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-07-25 01:33:02 +0530

    planet componet revamp

--- falcone/src/modules/Planets/index.js
+++ falcone/src/modules/Planets/index.js
@@ -4,47 +4,61 @@ import { planetImgConstant } from "../../constants/image-constants";
 import "./index.css";
 
 const Planets = props => {
-  const { currentDestination, availablePlanets } = props;
-  const [currentPlanet, addCurrentPlanet] = useState(""); // to show the current selected planet
+  const { currentDestination, activePlanet, planets, selectedPlanets } = props;
+  const [currentPlanet, addCurrentPlanet] = useState(activePlanet); // to show the current selected planet
+
+  // remove the launched planets
+  let availablePlanets = planets;
+  if (currentPlanet) {
+    availablePlanets = planets.filter(el => {
+      return !selectedPlanets.includes(el.name);
+    });
+  }
+
   return (
     <div className="available-planets-container">
-      Remaining planets where Al Falcone may hide
-      <div className="available-planets-wrapper inner-wrapper">
-        {availablePlanets &&
-          availablePlanets.map(el => {
-            return (
-              <div
-                onClick={() => {
-                  addCurrentPlanet(el.name);
-                  props.setCurrentPlanet(el.name);
-                }}
-                key={currentDestination + "-" + el.name}
-                className={`available-planet ${
-                  el.name === currentPlanet ? "selected-planet" : ""
-                }`}
-                ref={planet => {
-                  // scroolintoview for selected element
-                  if (el.name === currentPlanet) {
-                    planet &&
-                      planet.scrollIntoView({
-                        behavior: "smooth",
-                        inline: "center"
-                      });
-                  }
-                }}
-              >
-                <img src={planetImgConstant[el.name]} />
-                <div
-                  className={`${
-                    el.name === currentPlanet ? "selected-" : ""
-                  }planet-name`}
-                >
-                  {el.name}
-                </div>
-              </div>
-            );
-          })}
+      <div className="destination-info">
+        Destination {currentDestination + 1}
       </div>
+      <div className="available-planets-wrapper inner-wrapper">
+        <div className="available-planets-sub-wrapper">
+          {availablePlanets &&
+            availablePlanets.map(el => {
+              return (
+                <div
+                  onClick={() => {
+                    addCurrentPlanet(el.name);
+                    props.setCurrentPlanet(el.name);
+                  }}
+                  key={currentDestination + "-" + el.name}
+                  className="available-planet"
+                  ref={planet => {
+                    // scroolintoview for selected element
+                    if (el.name === currentPlanet) {
+                      planet &&
+                        planet.scrollIntoView({
+                          behavior: "smooth",
+                          inline: "center"
+                        });
+                    }
+                  }}
+                >
+                  <img src={planetImgConstant[el.name]} />
+                  <div
+                    className={`${
+                      el.name === currentPlanet ? "selected-" : ""
+                    }planet-name`}
+                  >
+                    {el.name}
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      {/* <div className="vehicles-wrapper">
+        <Vehicles currentPlanet={currentPlanet} />
+      </div> */}
     </div>
   );
 };
@@ -53,7 +67,9 @@ const mapDispatchToProps = {};
 
 const mapStateToProps = state => {
   return {
-    currentDestination: state.currentDestination
+    currentDestination: state.currentDestination,
+    planets: state.planets,
+    selectedPlanets: state.selectedPlanets
   };
 };
 
