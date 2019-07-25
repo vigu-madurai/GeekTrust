8e30cd04e3ca4042f203e882a94cf90b6e389b76
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-07-26 00:45:30 +0530

    status code refactored

--- falcone/src/modules/Planets/index.js
+++ falcone/src/modules/Planets/index.js
@@ -1,80 +1,75 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { planetImgConstant } from "../../constants/image-constants";
-import "./index.css";
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+import { planetImgConstant } from '../../constants/image-constants';
+import './index.css';
 
 const Planets = props => {
-  const { currentDestination, activePlanet, planets, selectedPlanets } = props;
-  const [currentPlanet, addCurrentPlanet] = useState(activePlanet); // to show the current selected planet
+	const { currentDestination, activePlanet, planets, selectedPlanets } = props;
+	const [currentPlanet, addCurrentPlanet] = useState(activePlanet); // to show the current selected planet
 
-  // remove the launched planets
-  let availablePlanets = planets;
-  if (currentPlanet) {
-    availablePlanets = planets.filter(el => {
-      return !selectedPlanets.includes(el.name);
-    });
-  }
+	// remove the launched planets
+	let availablePlanets = planets;
+	if (currentPlanet) {
+		availablePlanets = planets.filter(el => {
+			return !selectedPlanets.includes(el.name);
+		});
+	}
 
-  return (
-    <div className="available-planets-container">
-      Select any four planets you want to search in...
-      <div className="destination-info">
-        Destination {currentDestination + 1}
-      </div>
-      <div className="available-planets-wrapper inner-wrapper">
-        <div className="available-planets-sub-wrapper">
-          {availablePlanets &&
-            availablePlanets.map(el => {
-              return (
-                <div
-                  onClick={() => {
-                    addCurrentPlanet(el.name);
-                    props.setCurrentPlanet(el.name);
-                  }}
-                  key={currentDestination + "-" + el.name}
-                  className="available-planet"
-                  ref={planet => {
-                    // scroolintoview for selected element
-                    if (el.name === currentPlanet) {
-                      planet &&
-                        planet.scrollIntoView({
-                          behavior: "smooth",
-                          inline: "center"
-                        });
-                    }
-                  }}
-                >
-                  <img src={planetImgConstant[el.name]} />
-                  <div
-                    className={`${
-                      el.name === currentPlanet ? "selected-" : ""
-                    }planet-name`}
-                  >
-                    {el.name}
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-      </div>
-      {/* <div className="vehicles-wrapper">
-        <Vehicles currentPlanet={currentPlanet} />
-      </div> */}
-    </div>
-  );
+	return (
+		<div className='available-planets-container'>
+			Select any four planets you want to search in...
+			<div className='destination-info'>Destination {currentDestination + 1}</div>
+			{/* Planet wrappers */}
+			<div className='available-planets-wrapper inner-wrapper'>
+				<div className='available-planets-sub-wrapper'>
+					{availablePlanets &&
+						availablePlanets.map(el => {
+							return (
+								<div
+									onClick={() => {
+										addCurrentPlanet(el.name);
+										props.setCurrentPlanet(el.name);
+									}}
+									key={currentDestination + '-' + el.name}
+									className='available-planet'
+									ref={planet => {
+										// scroolintoview for selected element
+										if (el.name === currentPlanet) {
+											planet &&
+												planet.scrollIntoView({
+													behavior: 'smooth',
+													inline: 'center'
+												});
+										}
+									}}
+								>
+									{/* set image for the planets */}
+									<img src={planetImgConstant[el.name]} />
+									{/* highlight the last selected plant */}
+									<div className={`${el.name === currentPlanet ? 'selected-' : ''}planet-name`}>
+										{el.name}
+									</div>
+								</div>
+							);
+						})}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const mapDispatchToProps = {};
 
 const mapStateToProps = state => {
-  return {
-    currentDestination: state.currentDestination,
-    planets: state.planets,
-    selectedPlanets: state.selectedPlanets
-  };
+	return {
+		currentDestination: state.currentDestination,
+		planets: state.planets,
+		selectedPlanets: state.selectedPlanets
+	};
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(Planets);
